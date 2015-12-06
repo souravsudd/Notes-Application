524d79d97f791ea4650e674eae17ca47e084254f
souravsudd <er.saurabhkumar19@gmail.com>
2015-12-06 23:03:28 +0530

    Dao file

---
+++ ErrorObject.java
@@ -0,0 +1,35 @@
+package com.gotprint.services.notes.bo;
+
+public class ErrorObject {
+
+	private String code;
+	
+	private String message;
+	
+	private String field;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+	
+}
